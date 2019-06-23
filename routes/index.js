298fb4fdbbe12a3279fb1aa0e8045472c663a6a3
00127417 <00127417@uca.edu.sv>
2019-06-22 23:44:17 -0600

    rutas y vistas

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/politica', function(req, res, next) {
+  res.render('politica');
 });
 
 module.exports = router;
